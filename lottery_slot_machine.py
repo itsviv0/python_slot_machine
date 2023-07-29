6cf3fa646e1b9ef25cd113e4ca5c49127989fb93
itsviv0 <vivekganeshs12@gmail.com>
2023-07-29 17:21:47 +0530

    added 5 functions

---
+++ lottery_slot_machine.py
@@ -0,0 +1,79 @@
+# this is a slot machine project
+
+# this module is used to generate random numbers for the slot machine
+import random
+
+# the global variables which are common to the whole of 
+MAX_LINES = 3
+MIN_BET = 1
+MAX_BET = 10
+
+# this function takes user input for the amount to be deposited in the slot machine
+def deposit():
+    while True:
+    # to check if the entered value is a positive number
+        amount = input("Enter amount to deposit: $")
+        if amount.isdigit():    
+            amount = int(amount)
+            if amount >= 0:
+                break
+            else:
+                print("Amount must be greater than zero!")
+        else:
+            print("Please enter a valid amount.")
+         # the loop continues until the user enters a positive integer
+    return amount
+
+# this function takes user input for the number of lines to bet on
+def get_number_of_lines():
+    # checks if the number of lines is a positive number and in range
+    while True:
+        lines = input("Enter number of lines to bet on\nAvailabel lines 1 - " + str(MAX_LINES) + " : ")
+        if lines.isdigit():
+            lines = int(lines)
+            if (MAX_LINES >= lines >= 1):
+                break
+            else:
+                print("Please enter the lines in range!")
+        else:
+            print("Please enter a valid number")
+    # the loop continues until the user enters a positive integer in range
+    return lines
+
+#function to get the betting amount from user for each line
+def get_bet_amount(cur_amt):
+    # checks if the entered amount can be put to bet
+    while True:
+        bet_amt = input("Enter the bet amount on each line: $")
+        if bet_amt.isdigit():
+            bet_amt = int(bet_amt)
+            if ((MIN_BET <= bet_amt <= MAX_BET) and bet_amt <= cur_amt) :
+                break
+            else:
+                if cur_amt < MAX_BET:
+                    cur_max = cur_amt
+                else:
+                    cur_max_max = MAX_BET
+                print(f"Bet amount must be between ${MIN_BET} - ${cur_max}.")
+        else:
+            print("Enter a valid bet amount")
+        # runs until user enters a valid bet amount
+    return bet_amt
+
+# this function displays the current balance of the player
+def display_cur_balance(cur_amt):
+    print("Your balance amount is $" + str(cur_amt))
+
+
+
+# the main function is called slot_machine
+def slot_machine():
+    current_amount = deposit()
+    lines_on_bet = get_number_of_lines()
+    bet_amount = get_bet_amount(current_amount)
+    display_cur_balance(current_amount)
+
+
+
+# running the main function 
+slot_machine()  
