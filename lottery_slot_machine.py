90757d152190e94005a0636e9f98af7ad7d09469
itsviv0 <vivekganeshs12@gmail.com>
2024-03-24 15:45:04 +0530

    completed get_bet_amount function

--- lottery_slot_machine.py
+++ lottery_slot_machine.py
@@ -8,6 +8,42 @@ MAX_LINES = 3
 MIN_BET = 1
 MAX_BET = 10
 
+#number of rows and columns in the slot machine
+ROWS = 3
+COLS = 3
+
+symbols = {
+    'w' : 6,
+    'x' : 2,
+    'y' : 4,
+    'z' : 5
+}
+
+#this function is to generate symbols for each column based on the dictionary "symbols"
+def slotmachine_spin(rows, cols, symbols):
+    all_symbols = []
+    #add all the symbols to the list "symbols"
+    for symbols, symbol_count in symbols.items():
+        for _ in range(symbol_count):
+            all_symbols.append(symbol)
+    columns = []
+    for _ in range(cols):
+        column = []
+        #make a copy of all symbols 
+        current_symbols = all_symbols[:]
+        #generating row symbols
+        for _ in range(rows):
+            value = random.choice(current_symbols)
+            current_symbols.remove(value)
+            column.append(value)
+        columns.append(column)
+
+    return columns
+
+#printing the columns in transposed order
+def display_slot_machine_spin(columns):
+    for row in range(len(columns[0]))
+
 # this function takes user input for the amount to be deposited in the slot machine
 def deposit():
     while True:
